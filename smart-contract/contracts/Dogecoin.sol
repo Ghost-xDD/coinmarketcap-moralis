fb2dd9b9b557e19b96ca1bb6ad808cdeb90d699c
Ademola Adebowale <ademolajohn844@gmail.com>
2022-06-19 21:06:20 +0100

    update settings

--- smart-contract/contracts/Dogecoin.sol
+++ smart-contract/contracts/Dogecoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DogeCoin is ERC20, Ownable {
+contract DogeCoin is ERC20, Ownable { 
     constructor() ERC20("DogeCoin", "DOGE") {}
 
     function mint(address to, uint256 amount) public payable {
